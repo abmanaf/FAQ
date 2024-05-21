dc87dc8772cf0d8ec9a187070b3539e788277896
Alibaba <abdulmanafaliu414@gmail.com>
2024-05-21 14:23:42 +0000

    Changes made

---
+++ script.js
@@ -0,0 +1,88 @@
+"use strict";
+
+window.addEventListener("load", function () {
+  // Variables
+  const faqCard = document.querySelector(".faq-card");
+  const faqList = document.querySelector(".faq-list");
+  const faqItems = [...document.querySelectorAll(".faq-item")];
+  const faqItemHeaders = [...document.querySelectorAll(".faq-item-header")];
+
+  // Functions
+  const displayActiveIcon = function (activeIcon, inactiveIcon) {
+    activeIcon.style.display = "block";
+    inactiveIcon.style.display = "none";
+  };
+
+  const closeFaqItems = function () {
+    faqItems.forEach((faqItem) => {
+      const closeHeight =
+        faqItem.querySelector(".faq-item-header").scrollHeight;
+      faqItem.style.maxHeight = `${closeHeight}px`;
+      faqItem.dataset.openFaqList = false;
+      displayActiveIcon(
+        faqItem.querySelector(".plus-icon"),
+        faqItem.querySelector(".minus-icon")
+      );
+    });
+  };
+
+  closeFaqItems();
+
+  // Faq Item Click Event
+  faqList.addEventListener("click", function (event) {
+    if (event.target.closest(".faq-item-header")) {
+      const targetFaqItem = event.target
+        .closest(".faq-item-header")
+        .closest(".faq-item");
+      if (targetFaqItem.dataset.openFaqList === "false") {
+        closeFaqItems();
+        targetFaqItem.style.maxHeight = `${targetFaqItem.scrollHeight}px`;
+        targetFaqItem.dataset.openFaqList = true;
+        displayActiveIcon(
+          targetFaqItem.querySelector(".minus-icon"),
+          targetFaqItem.querySelector(".plus-icon")
+        );
+      } else {
+        closeFaqItems();
+      }
+    }
+  });
+
+  faqList.addEventListener("keydown", function (event) {
+    if (event.target.closest(".faq-item-header") && event.keyCode === 13) {
+      const faqItem = event.target.closest(".faq-item");
+      if (faqItem.dataset.openFaqList === "false") {
+        closeFaqItems();
+        faqItem.style.maxHeight = `${faqItem.scrollHeight}px`;
+        faqItem.dataset.openFaqList = true;
+        displayActiveIcon(
+          faqItem.querySelector(".minus-icon"),
+          faqItem.querySelector(".plus-icon")
+        );
+      } else {
+        closeFaqItems();
+      }
+    }
+  });
+
+  // Resize Observer
+  let initialFaqCardWidth = faqCard.offsetWidth;
+
+  const resizeObserver = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      const { width } = entry.contentRect;
+      if (initialFaqCardWidth !== width) {
+        faqItems.forEach((faqItem) => {
+          if (faqItem.dataset.openFaqList === "false") {
+            const faqItemHeader = faqItem.querySelector(".faq-item-header");
+            faqItem.style.maxHeight = `${faqItemHeader.offsetHeight}px`;
+          } else {
+            faqItem.style.maxHeight = `${faqItem.scrollHeight}px`;
+          }
+        });
+      }
+    }
+  });
+
+  resizeObserver.observe(faqCard);
+});
